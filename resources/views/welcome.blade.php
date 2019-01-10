26202e448f2b09a9864b988fc7b9a3d3a1e4db7a
Nathaniel San Carlos <nathancarlos017@yahoo.com>
2019-01-10 16:13:49 +0800

    Added contents(about) in welcome page. Volunteer and Contact us form not yet working.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         <span style="font-weight:800; color: white; font-size: 3rem; background-color: #ff5345; padding: 1rem;">Get it FRESH</span>
     </section>
     <section class="section static">
-        <span id="login-section">Log in to Agrabah</span>
+        <span class="section-head" id="login-section">Log in to Agrabah</span>
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div class="form-group">
@@ -85,7 +85,7 @@
             </span>
         </div>
     </section>
-    <section class="section static">
+    <section class="section static1">
         <div class="section-text">
             <span>
             We want to show Filipino youth that farming is not just a noble profession; it can be a profitable one too. We want to encourage Filipinos to take more of an interest in where their food is coming from, who the farmer is that produced their food, and the ways and means it took to get their food from farm to table. We’re on a mission to put farming in the spotlight, leading the wave of change to help create a sustainable Agricultural Ecosystem here in the Philippines. We want to make things better for the next generation of Filipino Farmers.
@@ -111,7 +111,7 @@
         </div>
     </section>
     <section class="lg-section static">
-        <span id="login-section">Volunteer</span>
+        <span class="section-head">Volunteer</span>
         <form class="m-2">
             <div class="form-row">
                 <div class="form-group col-md-6">
@@ -155,6 +155,82 @@
             </div>
             <button class="btn btn-primary" type="submit">Submit</button>
         </form>
+    </section>
+    <section class="section static1">
+        <div class="section-text">
+            <h3>About</h3>
+            <br>
+            <span style="font-size:1rem;">
+            Agrabah Marketplace is more than a platform, we build communities. Part of our initiative is to create awareness and encourage millennials to take part in the wave of change. We want to create awareness around the topic of food sustainability in the Philippines. Everyone’s efforts can help create the ripple effect regardless of how big or small their input is. We aim to do this through volunteering efforts and also promoting agri-preneurship as a viable occupational choice. 
+            </span>
+        </div>
+    </section>
+    <section class="section parallax bg6">
+        <div style="padding:2rem; color:white; background:rgba(0,0,0,0.5);text-align:center;">
+            <span style="font-size:1.5rem;">There is a problem in the agricultural value chain. Filipino farmers get the smallest profit share while rendering a majority of the hard work.</span>
+        </div>
+    </section>
+    <section class="section static">
+        <div class="section-text">
+            <span style="font-size:1.2rem;">How do we connect the consumers directly to the producers and redistribute a fair profit share to the farmers?</span>
+            <br>
+            <span style="font-size:1rem;">
+            We created a b2b web-app platform that address this issue. We provide fisherman and farmers with discoverability which means an increase in sales, marketing cost reduction and elimination of vendor-client bias. On the other hand we also provide consumer’s product cost savings, vendor discovery and analytics.
+            </span>
+        </div>
+    </section>
+    <section class="section static1">
+        <div class="section-text" style="text-align:left; align:left; align-items:baseline;">
+            <h3>Impact</h3>
+            <br>
+            <h5>Farmers</h5>
+            <ul>
+                <li>Stable Market Channel</li>
+                <li>Get connected to other farmers to learn best practices</li>
+                <li>Get access to public and private support</li>
+            </ul>
+            <h5>Local Business</h5>
+            <ul>
+                <li>Have an overview of the supply</li>
+                <li>Establish a market standard for quality</li>
+                <li>Establish a market standard for quality</li>
+            </ul>
+            <h5>Government</h5>
+            <ul>
+                <li>Get access to supply availability and market demand data (Generate Predictive Analytics)</li>
+            </ul>
+        </div>
+    </section>
+    <section class="lg-section static">
+        <span class="section-head">Contact Us</span>
+        <form class="m-2">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="contactFirstName">First Name</label>
+                    <input type="text" id="contactFirstName" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="contactLastName">Last Name</label>
+                    <input type="text" id="contactLastName" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="contactEmail">Email</label>
+                <input type="text" class="form-control" id="contactEmail">
+            </div>
+            <div class="form-group">
+                <label for="contactMessage">Message</label>
+                <textarea class="form-control" id="contactMessage" rows="3"></textarea>
+            </div>
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </form>
+    </section>
+    <section class="welcome-footer">
+                Address: <br>
+                Institute of Management – Dado Banatao Incubator <br>
+                123 Paseo de Roxas, Legazpi Village, Makati, 1229 Metro Manila <br>
+                <br>
+                Agrabah Marketplace &copy; 2019
     </section>
 </div>
 @endsection
